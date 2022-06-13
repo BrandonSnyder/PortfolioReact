8f8f77cb3f4ff58efcad2deb384f3912144ae559
Brandon Snyder <brandon.snyder019@gmail.com>
2022-06-13 11:49:12 -0400

    test changes

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -34,12 +34,7 @@ export default function AboutMe() {
           Vessels back into the fleet on time and under budget.
         </h5>
         <div className="resumeContainer">
-          <a
-            href="https://drive.google.com/file/d/1HVLL1NnDY48M19PEnfc9_FXe_lsJbN2y/view?usp=sharing"
-            download
-          >
-            Click to Download Resume
-          </a>
+          
         </div>
       </div>
     </section>
